cd2304f6500f570044271e448f13ab0b06292a70
Marc Arndt <marc.arndt@masimi.de>
2017-10-13 14:18:16 +0200

    - add handsontable - add a handsontable component to the show file content step in the wizard

--- lending-admin-client/src/app/app.module.ts
+++ lending-admin-client/src/app/app.module.ts
@@ -7,20 +7,21 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {OverviewComponent} from './overview/overview.component';
 import {AddSingeGameComponent} from './add-singe-game/add-singe-game.component';
 import {BarcodeValidatorDirective} from './validators/barcode-validator.directive';
-import {FormsModule} from "@angular/forms";
-import {GameService} from "./services/game.service";
-import {HttpClientModule} from "@angular/common/http";
-import {BarcodeService} from "./services/barcode.service";
-import { ShowAllGamesComponent } from './show-all-games/show-all-games.component';
-import {Ng2SmartTableModule} from "ng2-smart-table";
-import { DropZoneDirective } from './drop-zone.directive';
-import { AddMultipleGamesComponent } from './add-multiple-games/add-multiple-games.component';
-import {ArchwizardModule} from "ng2-archwizard";
+import {FormsModule} from '@angular/forms';
+import {GameService} from './services/game.service';
+import {HttpClientModule} from '@angular/common/http';
+import {BarcodeService} from './services/barcode.service';
+import {ShowAllGamesComponent} from './show-all-games/show-all-games.component';
+import {Ng2SmartTableModule} from 'ng2-smart-table';
+import {DropZoneDirective} from './drop-zone.directive';
+import {AddMultipleGamesComponent} from './add-multiple-games/add-multiple-games.component';
+import {ArchwizardModule} from 'ng2-archwizard';
 import {DndModule} from 'ng2-dnd';
-import { FileSelectionStepComponent } from './file-selection-step/file-selection-step.component';
-import { SheetSelectionStepComponent } from './sheet-selection-step/sheet-selection-step.component';
-import { CheckSheetContentStepComponent } from './check-sheet-content-step/check-sheet-content-step.component';
-import { ColumnAssignmentStepComponent } from './column-assignment-step/column-assignment-step.component';
+import {FileSelectionStepComponent} from './file-selection-step/file-selection-step.component';
+import {SheetSelectionStepComponent} from './sheet-selection-step/sheet-selection-step.component';
+import {CheckSheetContentStepComponent} from './check-sheet-content-step/check-sheet-content-step.component';
+import {ColumnAssignmentStepComponent} from './column-assignment-step/column-assignment-step.component';
+import {HotTableModule} from 'angular-handsontable';
 
 @NgModule({
   declarations: [
@@ -44,7 +45,8 @@ import { ColumnAssignmentStepComponent } from './column-assignment-step/column-a
     NgbModule.forRoot(),
     Ng2SmartTableModule,
     ArchwizardModule,
-    DndModule.forRoot()
+    DndModule.forRoot(),
+    HotTableModule
   ],
   providers: [GameService, BarcodeService],
   bootstrap: [AppComponent]
