22a7f53e1719e6fa794ed83415f14bde81e2d6bb
Marc Arndt <marc.arndt@masimi.de>
2017-11-12 13:59:31 +0100

    - add a websocket and a scheduler to the lending admin server, which pushes overview updates to the clients

---
+++ lending-admin-server/src/main/scala/info/armado/ausleihe/model/Clients.scala
@@ -0,0 +1,40 @@
+package info.armado.ausleihe.model
+
+import java.util.concurrent.ConcurrentHashMap
+import javax.enterprise.context.ApplicationScoped
+import javax.websocket.Session
+
+@ApplicationScoped
+class Clients {
+  var lastUpdate: Option[OverviewUpdate] = None
+
+  val sessions = new ConcurrentHashMap[Session, Unit]
+
+  def sendUpdate(update: OverviewUpdate): Unit = {
+    this.lastUpdate = Option(update)
+
+    this.sessions.forEach((session, _) => sendToSession(session, update))
+  }
+
+  def addUser(session: Session): Unit = {
+    this.lastUpdate.foreach(sendToSession(session, _))
+
+    this.sessions.put(session, Unit)
+  }
+
+  private def sendToSession(session: Session, update: OverviewUpdate): Unit =
+    session.getBasicRemote.sendText(update.toJSON)
+}
+
+class OverviewUpdate(numberOfGames: Long, numberOfLendGames: Long,
+                     numberOfIdentityCards: Long, numberOfLendIdentityCards: Long) {
+  def toJSON: String =
+    s"""
+       |{
+       |  "numberOfGames": ${numberOfGames},
+       |  "numberOfLendGames": ${numberOfLendGames},
+       |  "numberOfIdentityCards": ${numberOfIdentityCards},
+       |  "numberOfLendIdentityCards": ${numberOfLendIdentityCards}
+       |}
+    """.stripMargin
+}
